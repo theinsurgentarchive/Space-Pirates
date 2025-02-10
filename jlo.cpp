08163f87b248fd013068e23bda08afb18c2be680
justi <jlo2@csub.edu>
2025-02-10 15:47:52 -0800

    internal(Health,Position) added health and position

--- jlo.cpp
+++ jlo.cpp
@@ -1,75 +1,73 @@
 #include "jlo.h"
 #include <cmath>
 
-//Start - Vector2
-Vector2::Vector2() : pos{0,0} {}
+//Start - Vec2
+Vec2::Vec2() : pos{0,0} {}
 
-Vector2::Vector2(float x, float y) : pos{x,y} {}
+Vec2::Vec2(float x, float y) : pos{x,y} {}
 
-float Vector2::getX() {
+float Vec2::getX() {
     return pos[0];
 }
 
-float Vector2::getY() {
+float Vec2::getY() {
     return pos[1];
 }
 
-void Vector2::setX(float x) {
+void Vec2::setX(float x) {
     pos[0] = x;
 }
 
-void Vector2::setY(float y) {
+void Vec2::setY(float y) {
     pos[1] = y;
 }
 
-Vector2 Vector2::operator-() const {
-    return Vector2(-pos[0],-pos[1]);
+Vec2 Vec2::operator-() const {
+    return Vec2(-pos[0],-pos[1]);
 }
 
-float Vector2::operator[] (int i) const {
+float Vec2::operator[] (int i) const {
     return pos[i];
 }
 
-float& Vector2::operator[] (int i) {
+float& Vec2::operator[] (int i) {
     return pos[i];
 }
 
-Vector2& Vector2::operator+= (Vector2& v) {
+Vec2& Vec2::operator+= (Vec2& v) {
     pos[0] += v.pos[0];
     pos[1] += v.pos[1];
     return *this;
 }
 
-Vector2& Vector2::operator*= (float scale) {
+Vec2& Vec2::operator*= (float scale) {
     pos[0] *= scale;
     pos[1] *= scale;
     return *this;
 }
 
-float Vector2::length() const {
+float Vec2::length() const {
     return std::sqrt(length_squared());
 }
-float Vector2::length_squared() const {
+float Vec2::length_squared() const {
     return pos[0] * pos[0] + pos[1] * pos[1];
 }
-//End - Vector2 
+//End - Vec2 
 
-//Start - Positionable 
-IPositionable::IPositionable() : pos(0,0) {}
+//Start - Entity
+Entity::Entity(int i) : id(i) {}
 
-IPositionable::IPositionable(const Vector2& pos) : pos(pos) {}
-
-const Vector2& IPositionable::getPosition() const {
-    return pos;
+int Entity::getId() const {
+    return id;
 }
+//End - Entity
 
-void IPositionable::setPosition(float x, float y) {
-    pos.setX(x);
-    pos.setY(y);
-}
-//End - Positionable
+//Start - Health
+Health::Health(int h) : health(h) {}
+//End - Health
 
-
-
+//Start - Position
+Position::Position(float x, float y) : pos(x,y) {}
+//End - Position
 
 
--- jlo.h
+++ jlo.h
@@ -1,41 +1,50 @@
 #ifndef JLO_H
 #define JLO_H
 #include <cmath>
-class Vector2 {
+class Vec2 
+{
     private:
         float pos[2];
         float length_squared() const;
         
     public:
-        Vector2() {}
-        Vector2(float x, float y);
+        Vec2();
+        Vec2(float x, float y);
         float getX();
         float getY();
         void setX(float x);
         void setY(float y);
-        Vector2 operator-() const;
+        Vec2 operator-() const;
         float operator[] (int i) const;
         float& operator[] (int i);
-        Vector2& operator+= (Vector2& v);
-        Vector2& operator*= (float scale);
+        Vec2& operator+= (Vec2& v);
+        Vec2& operator*= (float scale);
         float length() const;
 
 };
 
-class IPositionable {
-    protected:
-        Vector2 pos;
-
+class Entity 
+{
+    private:
+        int id;
+    
     public:
-        IPositionable() {}
-        IPositionable(const Vector2& pos);
-        const Vector2& getPosition() const;
-        void setPosition(float x, float y);
+        Entity(int i);
+        int getId() const;
 
 };
 
-class Player : public IPositionable {
-
+struct Position
+{
+    Vec2 pos;
+    Position(float x = 0.f, float y = 0.f);
 };
+
+struct Health
+{
+    int health;
+    Health(int h = 0);
+};
+
 
 #endif JLO_H
