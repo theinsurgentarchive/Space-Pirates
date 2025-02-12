ac718c49ea91739f9a9f413dd43164f58a4449be
chufam <theinsurgentarchive@gmail.com>
2025-02-12 12:05:34 -0800

    removed unused definitions

--- dchu.cpp
+++ dchu.cpp
@@ -1,65 +1,8 @@
 #include "dchu.h"
 
-/*color Definitions*/
-color::color()
-{
-    red = 0;
-    blue = 0;
-    green = 0;
-}
-
-color::color(uint8_t new_red, uint8_t new_blue, uint8_t new_green)
-{
-    red = new_red;
-    blue = new_blue;
-    green = new_green;
-}
-
-uint8_t color::getRed()
-{
-    return red;
-}
-
-uint8_t color::getBlue()
-{
-    return blue;
-}
-
-uint8_t color::getGreen()
-{
-    return green;
-}
-
-void color::setColors(uint8_t new_red, uint8_t new_blue, uint8_t new_green)
-{
-    setRed(new_red);
-    setBlue(new_blue);
-    setGreen(new_green);
-}
-void color::setColors(color RGB)
-{
-    setRed(RGB.getRed());
-    setBlue(RGB.getBlue());
-    setGreen(RGB.getGreen());
-}
-
-void color::setRed(uint8_t new_red)
-{
-   red = new_red;
-}
-
-void color::setBlue(uint8_t new_blue)
-{
-   blue = new_blue;
-}
-
-void color::setGreen(uint8_t new_green)
-{
-   green = new_green;
-}
-/*End color Definitions*/
-
 /*planetBase Definitions*/
+
+/*
 planetBase::planetBase(EntityID id, ComponentMask mask) : Entity(id, mask)
 {
     is_exhausted = false;
@@ -106,5 +49,5 @@ void planetBase::exhausted()
     rgb.setColors(128, 128, 128);
 }
 
-
+*/
 /*End planetBase Definitions*/
