0ae7cf002fd0e6950555cdf7cada2f8a58ce3647
chufam <theinsurgentarchive@gmail.com>
2025-04-01 08:34:46 -0700

    More testing with operational positional check in check keys

--- asteroids.cpp
+++ asteroids.cpp
@@ -603,10 +603,10 @@ int check_keys(XEvent *e, World *w)
 					auto as = AStar{{0, 0}, wrd_size, {16.0f, 16.0f}};
 					v2u grid_size = as.size();
 					cout << grid_size[0] << ", " << grid_size[1] << endl;
-					if (as.getNode(4, 4) == nullptr) {
+					if (as.getNode(3, 3) == nullptr) {
 						cout << "Error, Cannot Find Node\n";
 					} else {
-						auto cn = as.getNode(4, 4)->getWorld();
+						auto cn = as.getNode(3, 3)->getWorld();
 						cout << cn[0] << ", " << cn[1] << "\n\n";
 						auto ct = w->tiles();
 						auto tct = ecs::ecs.component().fetch<TRANSFORM>(ct[1][1]);
