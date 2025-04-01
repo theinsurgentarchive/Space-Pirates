30d01d8643146f357a08b70e3b2287c128a85224
chufam <theinsurgentarchive@gmail.com>
2025-04-01 07:26:55 -0700

    fixed getNode function call in check keys

--- asteroids.cpp
+++ asteroids.cpp
@@ -601,7 +601,7 @@ int check_keys(XEvent *e, World *w)
 					break;
 				case XK_c:
 					auto as = AStar{{0, 0}, wrd_size, {16.0f, 16.0f}};
-					auto cn = as->getNode(0, 0)->getWorld();
+					auto cn = as.getNode(0, 0)->getWorld();
 					cout << cn[0] << ", " << cn[1] << "\n\n";
 					auto ct = w->tiles();
 					auto tct = ecs::ecs.component().fetch<TRANSFORM>(ct[0][0]);
