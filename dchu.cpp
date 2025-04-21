6b3d64b24960e8d18d44325b8f9b2bbaafd3b75e
Chufam <theinsurgentarchive@gmail.com>
2025-04-20 18:16:11 -0700

    added checking for if entity is on either target x or y, or both

--- dchu.cpp
+++ dchu.cpp
@@ -407,11 +407,15 @@ void moveTo(ecs::Entity* ent, v2f target)
     ) {
         return;
     }
-    if (dir[0] != 0.0f) {
-        physics->vel[0] = (-10.0f * dir[0]);
+    if (dir[0] != 0.0f && !(transform->pos[0] == target[0])) {
+        physics->vel[0] = (10.0f * dir[0]);
+    } else {
+        physics->vel[0] = 0.0f;
     }
-    if (dir[1] != 0.0f) {
-        physics->vel[1] = (-10.0f * dir[1]);
+    if (dir[1] != 0.0f && !(transform->pos[1] == target[1])) {
+        physics->vel[1] = (10.0f * dir[1]);
+    } else {
+       physics->vel[1] = 0.0f;
     }
 }
 
