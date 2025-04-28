e7bd99eb2c604d7a57b59bcd9cf667c0e8902e84
chufam <theinsurgentarchive@gmail.com>
2025-04-28 10:20:22 -0700

    removed deleted variable calls from Enemy constructor

--- dchu.cpp
+++ dchu.cpp
@@ -480,9 +480,7 @@ Enemy::Enemy(ecs::Entity* ent)
 Enemy::Enemy(ecs::Entity* ent, u16 atk, u16 path)
 {
     atk_Timer_Max = atk;
-    atk_Timer = 0;
     path_Timer_Max = path;
-    path_Timer = 0;
     do_damage = true;
     this->ent = ent;
     initEnemy();
