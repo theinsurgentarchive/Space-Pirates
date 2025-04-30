9008cc2785c91b7b1079456b3559c9424247a2d8
Chufam <theinsurgentarchive@gmail.com>
2025-04-30 15:58:13 -0700

    pathfinding operational, tweaked path_timer values

--- dchu.cpp
+++ dchu.cpp
@@ -485,7 +485,7 @@ void moveTo(ecs::Entity* ent, ecs::Entity* target)
     moveTo(ent, tar->pos);
 }
 
-Enemy::Enemy(ecs::Entity* ent) : Enemy(ent, {0.1f, 6.0f})
+Enemy::Enemy(ecs::Entity* ent) : Enemy(ent, {0.1f, 2.0f})
 {}
 
 Enemy::Enemy(ecs::Entity* ent, v2f t_mod)
