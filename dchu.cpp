071d4e4dad5a0316a2dbfcf54b35bc1df563745e
Chufam <theinsurgentarchive@gmail.com>
2025-05-07 02:23:04 -0700

    fixed static cast of cells in dchu.cpp

--- dchu.cpp
+++ dchu.cpp
@@ -607,8 +607,8 @@ void Enemy::initEnemy(World* w, float mag = 48.0f)
     }
     auto cells = w->cells;
     v2f w_max = {
-        static_cast<float>cells.size(),
-        static_cast<float>cells[0].size()
+        static_cast<float>(cells.size(),)
+        static_cast<float>(cells[0].size())
     };
     w_max[0] *= m_mag;
     w_max[1] *= m_mag;
